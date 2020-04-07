3b1923781390eb7aeef9465d4ff07d9f030a2946
Bastien BC <bastien.barneoud-chapelier@edu.devinci.fr>
2020-04-07 16:11:57 +0200

    Added PrefabSpawner on runtime, timeScaleEffect (slowmo,fastmo)

---
+++ Projet_Jam3D_IIM_Team1/Assets/Script/BastienScript/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState { MAIN_MENU, IN_GAME }
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+
+    //define a list
+    public static List<GameObject> myListObjects = new List<GameObject>();
+    //Number of objects spawned
+    public static int numSpawned = 0;
+    [SerializeField] private int numToSpawn = 5;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Important note: place your prefabs folder(or levels or whatever) 
+        //in a folder called "Resources" like this "Assets/Resources/Prefabs"
+        Object[] subListObjects = Resources.LoadAll("Prefabs", typeof(GameObject));
+        //This may be sloppy
+        foreach (GameObject subListObject in subListObjects)
+        {
+            GameObject lo = (GameObject)subListObject;
+
+            myListObjects.Add(lo);
+        }
+        Vector3 startPosition = transform.position;
+    }
+
+    void SpawnRandomObject()
+    {
+        //spawns item in array position between 0 and 100
+        //int whichItem = Random.Range(0, 100);
+
+        GameObject myObj = Instantiate(myListObjects[0]) as GameObject;
+        //myObj.GetComponent<Rigidbody>().isKinematic = true;
+
+        numSpawned++;
+
+        myObj.transform.position = transform.position;
+    }
+
+    void Update()
+    {
+        if (numToSpawn > numSpawned)
+        {
+            //where your instantiated object spawns from
+            transform.position = new Vector3(0, 10, 0);
+            SpawnRandomObject();
+        }
+    }
+}
---
+++ Projet_Jam3D_IIM_Team1/Assets/Script/BastienScript/InteractionManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+internal enum effect
+{
+    SLOWMO,
+    FASTMO
+}
+
+[RequireComponent(typeof(BoxCollider))]
+public class InteractionManager : MonoBehaviour
+{
+    [SerializeField] private effect effectType = effect.SLOWMO;
+    [Range (1,5)] [SerializeField] private float effectDuration = 1;
+
+    //https://answers.unity.com/questions/1284988/custom-inspector-2.html
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+            StartCoroutine("TimeScaleEffect", effectDuration);
+    }
+
+    private IEnumerator TimeScaleEffect(float timer)
+    {
+        if(effectType == effect.SLOWMO)
+        {
+            Time.timeScale = 0.5f;
+        }
+        if(effectType == effect.FASTMO)
+        {
+            Time.timeScale = 1.5f;
+        }
+        yield return new WaitForSeconds(timer);
+        Time.timeScale = 1f;
+    }
+}
