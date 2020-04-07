c3fc8783176ab4c8d953798aec6c8223c7267b9b
Bastien BC <bastien.barneoud-chapelier@edu.devinci.fr>
2020-04-07 16:57:54 +0200

    Effect zone are now easier to see and get be trigger after a certain time

--- Projet_Jam3D_IIM_Team1/Assets/Script/BastienScript/InteractionManager.cs
+++ Projet_Jam3D_IIM_Team1/Assets/Script/BastienScript/InteractionManager.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 internal enum effect
 {
@@ -16,18 +13,20 @@ public class InteractionManager : MonoBehaviour
 {
     [SerializeField] private effect effectType = effect.SLOWMO;
     [Range (1,5)] [SerializeField] private float effectDuration = 1;
+    [SerializeField] private float timeBeforeEffect = 0f;
 
     //https://answers.unity.com/questions/1284988/custom-inspector-2.html
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
-            StartCoroutine("TimeScaleEffect", effectDuration);
+            StartCoroutine(TimeScaleEffect(effectDuration, timeBeforeEffect));
     }
 
-    private IEnumerator TimeScaleEffect(float timer)
+    private IEnumerator TimeScaleEffect(float timer, float countdown)
     {
-        if(effectType == effect.SLOWMO)
+        yield return new WaitForSeconds(countdown);
+        if (effectType == effect.SLOWMO)
         {
             Time.timeScale = 0.5f;
         }
@@ -37,5 +36,12 @@ public class InteractionManager : MonoBehaviour
         }
         yield return new WaitForSeconds(timer);
         Time.timeScale = 1f;
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow cube at the transform position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, transform.GetComponent<BoxCollider>().size);
     }
 }
