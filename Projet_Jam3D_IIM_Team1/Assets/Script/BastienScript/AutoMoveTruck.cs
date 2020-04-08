155c3e4a1fed49c26412b06bf7b1331d23ef8501
Bastien BC <bastien.barneoud-chapelier@edu.devinci.fr>
2020-04-08 15:51:45 +0200

    Scene 1 base

---
+++ Projet_Jam3D_IIM_Team1/Assets/Script/BastienScript/AutoMoveTruck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMoveTruck : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+    }
+}
