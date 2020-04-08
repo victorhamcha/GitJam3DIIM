17c8fe040428c2bdb33da7f7118c2aea6f2d40c7
Hamcha <victor.hamcha@gmail.com>
2020-04-08 14:08:01 +0200

    Scene3

---
+++ Projet_Jam3D_IIM_Team1/Assets/Script/Victor/Scene3Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene3Manager : MonoBehaviour
+{
+    public GameObject poulet;
+    private int direction;
+    public float timerValue;
+    private float timer;
+    public Transform four;
+    private bool go=false;
+    public float throwPower;
+    private float trajectoire=0;
+    void Start()
+    {
+        timer = timerValue;
+        direction = Random.Range(-2, 2);
+        trajectoire = 50 * direction;
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        if(timer>0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if(!go)
+        {
+            go = true;
+          
+            GameObject bombe= Instantiate(poulet, four.position, Quaternion.identity);
+            bombe.GetComponent<Rigidbody>().AddForce(new Vector3(-120*throwPower, 110*throwPower, trajectoire));
+           
+           
+        }
+    }
+}
